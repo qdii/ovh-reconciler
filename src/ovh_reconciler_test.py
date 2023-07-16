23de33a227b4832735f2b041066798eeb5bd0e29
Victor Lavaud <victor.lavaud@pm.me>
2023-07-16 12:15:33 +0200

    Test that invalid input returns None

--- src/ovh_reconciler_test.py
+++ src/ovh_reconciler_test.py
@@ -14,12 +14,19 @@ class TestReconciler(unittest.TestCase):
         ' foo.dodges.it   A   10.0.0.1 ',
         'foo.dodges.it\tA 10.0.0.1',
         ])
-    def testParseLine_ProducesValidARecord(self, line):
+    def testParseLineWithValidInput_ProducesValidARecord(self, line):
         """Tests that a simple line of DNS record produces the right output."""
         record = ovh_reconciler.parse_line(line)
         self.assertEqual(record.type, ovh_reconciler.Type.A)
         self.assertEqual(record.subdomain, 'foo.dodges.it')
         self.assertEqual(record.target, '10.0.0.1')
+
+    @parameterized.expand([
+        '', ' ', '\t', '# A 10.0.0.1', 'A 10.0.0.1',
+        ])
+    def testParseLineWithInvalidInput_ReturnsNone(self, line):
+        """Tests that invalid line do not return a Record object."""
+        self.assertIsNone(ovh_reconciler.parse_line(line))
 
 
 if __name__ == '__main__':
